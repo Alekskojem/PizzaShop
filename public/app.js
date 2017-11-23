57e714585daeeb32aea16509e79d47c2c218dbde
Aleksandr Kojemyako <alekskojem@gmail.com>
2017-11-23 17:48:34 +0200

    Add comments js vs ruby

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 function something()
 {
-	var x = window.localStorage.getItem('aaa');
+	var x = window.localStorage.getItem('aaa'); // x = hh['bbb'] так в ruby 
 	
-	x = x*1 + 1;									
+	x = x*1 + 1;	// x = x + 1 так в ruby								
 
-	window.localStorage.setItem('aaa', x);		
+	window.localStorage.setItem('aaa', x); // hh['bbb'] = x   так в ruby		
 
 	alert(x);
 }
