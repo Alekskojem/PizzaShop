c69eeac82311e921f632133adc67eb2b6de86a39
Aleksandr Kojemyako <alekskojem@gmail.com>
2017-11-24 09:31:08 +0200

    Change description some pizza

--- db/migrate/20171120152959_add_products.rb
+++ db/migrate/20171120152959_add_products.rb
@@ -15,15 +15,15 @@ class AddProducts < ActiveRecord::Migration[5.1]
   		:description => 'Hawaii Ananas Pizza', 
   		:price => 140,
   		:size => 40, 
-  		:is_spicy => false, 
-  		:is_veg => true, 
+  		:is_spicy => true, 
+  		:is_veg => false, 
   		:is_best_offer => false, 
   		:path_to_image => '/images/hawaii.jpg'
   		})
 
   	Product.create ({
   		:title => 'Vegpizza', 
-  		:description => 'Nice Vegetatian pizza', 
+  		:description => 'Nice Vegetarian pizza', 
   		:price => 130, 
   		:size => 40, 
   		:is_spicy => false, 
