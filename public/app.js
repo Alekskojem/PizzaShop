c42a069019e529e198d1ae151a98b8a6a404aa3b
alekskojem <alekskojem@gmail.com>
2017-11-28 21:23:52 +0200

    Update orders button

--- public/app.js
+++ public/app.js
@@ -19,17 +19,21 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
-	
-	//Вывод количества итемов в корзине
-	alert('Items in your cart: ' + cart_get_number_of_items());
+	update_orders_button();
 }
+//Вывод количества итемов в корзине
+//	alert('Items in your cart: ' + cart_get_number_of_items());
 
 function update_orders_input()
 {
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
 
-
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
 }
 
 function cart_get_number_of_items()
